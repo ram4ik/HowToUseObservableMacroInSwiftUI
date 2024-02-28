d49d2a601235dbf0f62603193eb5d0d8bbb84213
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-28 18:41:30 +0200

    Initial Commit

---
+++ HowToUseObservableMacroInSwiftUI/HowToUseObservableMacroInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseObservableMacroInSwiftUIApp.swift
+//  HowToUseObservableMacroInSwiftUI
+//
+//  Created by test on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseObservableMacroInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
